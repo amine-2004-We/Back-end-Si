d77b4bc950d1583479753d1d6f4a5ed79087187a
amine haddade <aminehddade479@gmail.com>
2026-03-30 00:04:43 +0100

    udate version of checkout code

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/api', function () {
     return view('welcome');
 });
+Route::get('/api/test', function () {
+    return response()->json(['statusss' => 'ok job ci/cd']);
+});
 
 Route::get('/api/healthz', function() {
     return response()->json(['statusss' => 'oksss']);
